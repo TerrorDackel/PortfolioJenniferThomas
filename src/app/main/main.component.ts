414074b2e8d4677089f05a72f97f2a45d3f2ed88
TerrorDackel <jenny.codes.007@gmail.com>
2025-02-19 15:27:15 +0100

    structur

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { WorkTogetherComponent } from './work-together/work-together.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { MyWorkComponent } from './my-work/my-work.component';
+import { MyProjectComponent } from './my-project/my-project.component';
+import { RefSectionComponent } from './ref-section/ref-section.component';
+import { ContactSectionComponent } from './contact-section/contact-section.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, WorkTogetherComponent, SkillSetComponent, MyWorkComponent, MyProjectComponent,RefSectionComponent, ContactSectionComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.sass'
 })
