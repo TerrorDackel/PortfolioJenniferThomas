64c6f01bf13d2ae909fce58ca64d0ed4f5706675
TerrorDackel <jenny.codes.007@gmail.com>
2025-02-19 16:25:14 +0100

    skill-set programm component

---
+++ src/app/main/skill-set/programm-box/programm-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-programm-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './programm-box.component.html',
+  styleUrl: './programm-box.component.sass'
+})
+export class ProgrammBoxComponent {
+
+}
