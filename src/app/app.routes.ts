89d916400d83a186b665a33b9443960b5a4f05be
TerrorDackel <jenny.codes.007@gmail.com>
2025-03-18 10:12:34 +0100

    folderstructur reworked

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { MainComponent } from './main/main.component';
 
 export const routes: Routes = [
+    { path: '', component: MainComponent},
     { path: 'legal-notice', component: LegalNoticeComponent }
 ];
