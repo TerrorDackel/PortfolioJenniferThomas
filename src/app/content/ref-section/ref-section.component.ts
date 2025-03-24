46d0b0a7de850d6a5171f563dddd60cf125ad8e4
TerrorDackel <jenny.codes.007@gmail.com>
2025-03-24 16:22:05 +0100

    ref

--- src/app/content/ref-section/ref-section.component.ts
+++ src/app/content/ref-section/ref-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RefComponent } from './ref/ref.component';
 
 @Component({
   selector: 'app-ref-section',
   standalone: true,
-  imports: [],
+  imports: [RefComponent],
   templateUrl: './ref-section.component.html',
   styleUrl: './ref-section.component.sass'
 })
