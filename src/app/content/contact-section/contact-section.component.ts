f710e95dd75fd89e6ecec3201f93efcd2a7a2969
TerrorDackel <jenny.codes.007@gmail.com>
2025-03-30 09:18:32 +0200

    contact

--- src/app/content/contact-section/contact-section.component.ts
+++ src/app/content/contact-section/contact-section.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
+import { NgClass, NgIf } from '@angular/common';
 import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-section',
   standalone: true,
-  imports: [ RouterLink, TranslatePipe, TranslateDirective, ReactiveFormsModule, HttpClientModule ],
+  imports: [ RouterLink, TranslatePipe, TranslateDirective, ReactiveFormsModule, HttpClientModule,  NgIf, NgClass ],
   templateUrl: './contact-section.component.html',
   styleUrl: './contact-section.component.sass'
 })
@@ -29,28 +30,31 @@ export class ContactSectionComponent {
     this.submitted = true;
     this.formSuccess = false;
     this.formError = false;
-
-    if (this.contactForm.valid) {
-      const formData = this.contactForm.value;
-
-      const body = {
-        name: formData.name,
-        email: formData.email,
-        message: formData.message
-      };
-
-      const formspreeEndpoint = 'https://formspree.io/f/xyzedkbw';
-
-      this.http.post(formspreeEndpoint, body).subscribe({
-        next: () => {
-          this.formSuccess = true;
-          this.contactForm.reset();
-          this.submitted = false;
-        },
-        error: () => {
-          this.formError = true;
-        }
-      });
+  
+    if (!this.contactForm.valid) {
+      return;
     }
+  
+    const formData = this.contactForm.value;
+  
+    const body = {
+      name: formData.name,
+      email: formData.email,
+      message: formData.message
+    };
+  
+    const formspreeEndpoint = 'https://formspree.io/f/xyzedkbw';
+  
+    this.http.post(formspreeEndpoint, body).subscribe({
+      next: () => {
+        this.formSuccess = true;
+        this.contactForm.reset();
+        this.submitted = false;
+      },
+      error: () => {
+        this.formError = true;
+      }
+    });
   }
+  
 }
