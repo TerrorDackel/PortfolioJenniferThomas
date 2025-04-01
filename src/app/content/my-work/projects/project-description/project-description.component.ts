777115bc0dd9e429ec651f3af546862178c75bb3
TerrorDackel <jenny.codes.007@gmail.com>
2025-04-01 09:08:57 +0200

    projects

--- src/app/content/my-work/projects/project-description/project-description.component.ts
+++ src/app/content/my-work/projects/project-description/project-description.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
 
 @Component({
@@ -9,24 +9,9 @@ import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
   styleUrl: './project-description.component.sass'
 })
 export class ProjectDescriptionComponent {
-  projects = [
-    {
-      name: 'Join',
-      usedProgramms: ['Angular | ', 'TypeScript | ', 'HTML | ', 'CSS | ', 'Firebase'],
-      description: 'TaTaskmanager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag-and-Drop, weise Benutzer und Kategorien zu.sk manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
-      gitLink: 'https://github.com/TerrorDackel/Join'
-    },
-    {
-      name: 'El Pollo Loco',
-      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS'],
-      description: 'Jump’n’Run-Spiel mit Wurf-Funktion auf objektorientierter Basis. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
-      gitLink: 'https://github.com/TerrorDackel/El-Pollo-Loco'
-    },
-    {
-      name: 'Pokedex',
-      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS | ', 'API'],
-      description: 'Basierend auf der PokéAPI – eine einfache Bibliothek zur Anzeige und Katalogisierung von Pokémon-Informationen.',
-      gitLink: 'https://github.com/TerrorDackel/Pokedex'
-    }
-  ];
+  @Input() name: string = '';
+  @Input() usedProgramms!: string[];
+  @Input() description: string = '';
+  @Input() gitLink: string = '';
+  @Input() projectImg: string = '';
 }
--- src/app/content/my-work/projects/projects.component.ts
+++ src/app/content/my-work/projects/projects.component.ts
@@ -10,6 +10,27 @@ import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
   styleUrl: './projects.component.sass'
 })
 export class ProjectsComponent {
-  projectImg = ['assets/img/join.jpg', 'assets/img/el-pollo-loco.jpg', 'assets/img/pokedex.jpg']
-  name = ['Join', 'El Pollo Loco', 'Pokedex']
+  projects = [
+    {
+      name: 'Join',
+      usedProgramms: ['Angular | ', 'TypeScript | ', 'HTML | ', 'CSS | ', 'Firebase'],
+      description: 'TaTaskmanager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag-and-Drop, weise Benutzer und Kategorien zu.sk manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      gitLink: 'https://github.com/TerrorDackel/Join',
+      projectImg: 'assets/img/join.jpg'
+    },
+    {
+      name: 'El Pollo Loco',
+      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS'],
+      description: 'Jump’n’Run-Spiel mit Wurf-Funktion auf objektorientierter Basis. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
+      gitLink: 'https://github.com/TerrorDackel/El-Pollo-Loco',
+      projectImg: 'assets/img/el-pollo-loco.jpg'
+    },
+    {
+      name: 'Pokedex',
+      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS | ', 'API'],
+      description: 'Basierend auf der PokéAPI – eine einfache Bibliothek zur Anzeige und Katalogisierung von Pokémon-Informationen.',
+      gitLink: 'https://github.com/TerrorDackel/Pokedex',
+      projectImg: 'assets/img/pokedex.jpg'
+    }
+  ];
 }
