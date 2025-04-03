14e7f126df5a6aa313e0fef8ae186edc8d0354d1
TerrorDackel <jenny.codes.007@gmail.com>
2025-04-03 23:32:04 +0200

    ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, NavigationEnd, Event as RouterEvent } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { FooterComponent } from './footer/footer.component';
 import { TranslateService, TranslatePipe, TranslateDirective } from "@ngx-translate/core";
-
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +15,16 @@ import { TranslateService, TranslatePipe, TranslateDirective } from "@ngx-transl
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'PortfolioJenniferThomas';
-  constructor(private translate: TranslateService) {
-    this.translate.addLangs(['de', 'en']);
-    this.translate.setDefaultLang('en');
-    this.translate.use('de');
+  constructor(translate: TranslateService, router: Router) {
+    translate.addLangs(['de', 'en']);
+    translate.setDefaultLang('en');
+    translate.use('de');
+
+    router.events.pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd)
+    ).subscribe(e => {
+      if (['/legal-notice', '/privacy-policy'].includes(e.urlAfterRedirects))
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
   }
 }
--- src/app/content/my-work/projects/projects.component.ts
+++ src/app/content/my-work/projects/projects.component.ts
@@ -13,7 +13,7 @@ export class ProjectsComponent {
   projects = [
     {
       name: 'Join',
-      usedProgramms: ['Angular | ', 'TypeScript | ', 'HTML | ', 'CSS | ', 'Firebase'],
+      usedProgramms: ['Angular | TypeScript | HTML | CSS | Firebase'],
       description: 'Taskmanager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag-and-Drop, weise Benutzer und Kategorien zu.sk manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
       gitLink: 'https://github.com/TerrorDackel/Join',
       projectImg: 'assets/img/join.jpg',
@@ -21,7 +21,7 @@ export class ProjectsComponent {
     },
     {
       name: 'Pollo Loco',
-      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS'],
+      usedProgramms: ['JavaScript | HTML | CSS'],
       description: 'Jump’n’Run-Spiel mit Wurf-Funktion auf objektorientierter Basis. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
       gitLink: 'https://github.com/TerrorDackel/El-Pollo-Loco',
       projectImg: 'assets/img/el-pollo-loco.jpg',
@@ -29,7 +29,7 @@ export class ProjectsComponent {
     },
     {
       name: 'Pokedex',
-      usedProgramms: ['JavaScript | ', 'HTML | ', 'CSS | ', 'API'],
+      usedProgramms: ['JavaScript | HTML | CSS | API'],
       description: 'Basierend auf der PokéAPI – eine einfache Bibliothek zur Anzeige und Katalogisierung von Pokémon-Informationen.',
       gitLink: 'https://github.com/TerrorDackel/Pokedex',
       projectImg: 'assets/img/pokedex.jpg',
