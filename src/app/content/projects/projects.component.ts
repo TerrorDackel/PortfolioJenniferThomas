12591075c3be75911618f389297e5e9c75eb1faf
TerrorDackel <jenny.codes.007@gmail.com>
2025-05-13 15:15:59 +0200

    changed style of my work and projects section

--- src/app/content/projects/projects.component.ts
+++ src/app/content/projects/projects.component.ts
@@ -18,7 +18,7 @@ export class ProjectsComponent {
       usedProgramms: ['Angular | TypeScript | HTML | CSS | Firebase'],
       description: 'Taskmanager inspiriert vom Kanban-System. Erstelle und organisiere Aufgaben per Drag-and-Drop, weise Benutzer und Kategorien zu.sk manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
       gitLink: 'https://github.com/TerrorDackel/Join',
-      projectImg: 'assets/img/join.jpg',
+      projectImg: 'assets/img/join.png',
       livetest: 'https://jennifer-thomas.developerakademie.net/Join/index.html'
     },
     {
@@ -26,7 +26,7 @@ export class ProjectsComponent {
       usedProgramms: ['JavaScript | HTML | CSS'],
       description: 'Jump’n’Run-Spiel mit Wurf-Funktion auf objektorientierter Basis. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen das verrückte Huhn zu kämpfen.',
       gitLink: 'https://github.com/TerrorDackel/El-Pollo-Loco',
-      projectImg: 'assets/img/el-pollo-loco.jpg',
+      projectImg: 'assets/img/el-pollo-loco.png',
       livetest: 'https://jennifer-thomas.developerakademie.net/ElPolloLoco/index.html'
     },
     {
@@ -34,7 +34,7 @@ export class ProjectsComponent {
       usedProgramms: ['JavaScript | HTML | CSS | API'],
       description: 'Basierend auf der PokéAPI – eine einfache Bibliothek zur Anzeige und Katalogisierung von Pokémon-Informationen.',
       gitLink: 'https://github.com/TerrorDackel/Pokedex',
-      projectImg: 'assets/img/pokedex.jpg',
+      projectImg: 'assets/img/pokedex.png',
       livetest: 'https://jennifer-thomas.developerakademie.net/Pokedex160125/index.html'
     }
   ];
