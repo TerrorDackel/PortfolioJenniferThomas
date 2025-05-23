6c3ec09dc7768824b7bb0b5a2f445ad1a556d759
TerrorDackel <jenny.codes.007@gmail.com>
2025-05-23 15:31:52 +0200

    added some style-changes my work and contact

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,25 @@ import { filter } from 'rxjs/operators';
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, 
-    // TranslatePipe, TranslateDirective
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+    selector: 'app-root',
+    standalone: true,
+    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, 
+        // TranslatePipe, TranslateDirective
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  constructor(translate: TranslateService, router: Router) {
-    translate.addLangs(['de', 'en']);
-    translate.setDefaultLang('en');
-    translate.use('de');
+    constructor(translate: TranslateService, router: Router) {
+        translate.addLangs(['de', 'en']);
+        translate.setDefaultLang('en');
+        translate.use('de');
 
-    router.events.pipe(
-      filter((e): e is NavigationEnd => e instanceof NavigationEnd)
-    ).subscribe(e => {
-      if (['/legal-notice', '/privacy-policy'].includes(e.urlAfterRedirects))
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-  }
+        router.events.pipe(
+        filter((e): e is NavigationEnd => e instanceof NavigationEnd)
+        ).subscribe(e => {
+        if (['/legal-notice', '/privacy-policy'].includes(e.urlAfterRedirects))
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        });
+    }
 }
