2947f1b180f150f45059d75a4c027ecf5f542f73
TerrorDackel <jenny.codes.007@gmail.com>
2025-07-17 16:50:58 +0200

    adjusted hero-component section styling – fixed alignment and corrected nesting

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,18 +8,18 @@ import { RefSectionComponent } from './ref-section/ref-section.component';
 import { ContactSectionComponent } from './contact-section/contact-section.component';
 
 @Component({
-  selector: 'app-content',
-  standalone: true,
-  imports: [
-    HeroComponent, 
-    WorkTogetherComponent, 
-    SkillSetComponent, 
-    MyWorkComponent, 
-    ProjectsComponent,
-    RefSectionComponent, 
-    ContactSectionComponent
-  ],
-  templateUrl: './content.component.html',
-  styleUrl: './content.component.sass'
+    selector: 'app-content',
+    standalone: true,
+    imports: [
+        HeroComponent, 
+        WorkTogetherComponent, 
+        SkillSetComponent, 
+        MyWorkComponent, 
+        ProjectsComponent,
+        RefSectionComponent, 
+        ContactSectionComponent
+    ],
+    templateUrl: './content.component.html',
+    styleUrl: './content.component.sass'
 })
 export class ContentComponent { }
